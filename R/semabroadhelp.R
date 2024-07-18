f40d38d519254c256bc0607317adf6dbb73cb386
o p na uni <philuni@MacBook-Air-von-Philipp.local>
2024-07-18 21:47:54 +0200

    cleared some unnecessary things, worked on submission

--- R/semabroadhelp.R
+++ R/semabroadhelp.R
@@ -10,7 +10,7 @@ semabroadhelp <- function () {
   help <- c(
     "aerialway: station",
     "aeroway: aerodrome, terminal",
-    "amenity: bar, biergarten, cafe, fast_food, food_court, ice_cream, pub, restaurant, dancing_school, university, language_school, library, surf_school, bicycle_rental, car_rental, atm, bank, hospital, dentist, pharmacy, cinema, nightclub, theatre, marketplace",
+    "amenity: bar, biergarten, cafe, fast_food, food_court, ice_cream, pub, restaurant, dancing_school, university, language_school, library, bicycle_rental, car_rental, atm, bank, hospital, dentist, pharmacy, cinema, nightclub, theatre, marketplace",
     "building: hotel, commercial, kiosk, supermarket, religious, museum, castle, pagoda",
     "historic: archaeological_site, memorial, church",
     "leisure: bathing_place, dance, fitness_centre",
